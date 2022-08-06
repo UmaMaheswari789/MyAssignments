e92bd992d14811cab61889582abd5c433fc334e3
UmaMaheswari789 <110728715+UmaMaheswari789@users.noreply.github.com>
2022-08-06 19:37:33 +0530

    Add files via upload
    Day 1

---
+++ day1/CheckIfTheNumberIsPositiveOrNegative.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class CheckIfTheNumberIsPositiveOrNegative {
+	public static void main(String[] args) 
+	{
+	int n = 35;
+	if (n>0)
+	{
+		System.out.println("The number is positive");
+		
+	}
+	else if (n<0)
+	{
+		System.out.println("The number is negative");
+		
+	}
+	else if(n==0)
+	{
+		System.out.println("The number is neither positive nor negative");
+	}
+
+}
+}
---
+++ day1/ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class ConvertANegativeNumberToPositiveNumber {
+	public static void main(String[] args) {
+		int n=-40;
+		if(n<0)
+		{
+			n=n+50;
+			System.out.println(n);
+		if (n>0)
+			{
+				System.out.println("The number is positive");
+	
+			
+	}
+
+		}
+	}
+}
+
+
